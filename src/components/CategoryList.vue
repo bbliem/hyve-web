fb4340dbd36d12214de1bd9abbca8b13ebf73589
Bernhard Bliem <bernhard@bliem.xyz>
2020-10-22 19:44:29 +0300

    Minor change to data fetching logic

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -20,27 +20,35 @@ export default {
     return {
       categories: [],
       error: null,
-      loading: true,
+      loading: false,
     }
   },
   created: function() {
-    fetch(this.$appConfig.backendApiUrl + this.$appConfig.endpointCategory)
-      .then(response => response.json())
-      .then(json => {
-        this.categories = json.map(category => ({
-          id: category.id,
-          title: category.name,
-          contents: category.lessons.map(lesson =>({
-            id: lesson.id,
-            title: lesson.name,
-          })),
-        }))
-      })
-      .catch(error => {
-        this.error = 'Could not load categories: ' + error
-        console.error(this.error)
-      })
-      .finally(() => this.loading = false)
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.categories = []
+      this.error = null
+      this.loading = true
+      fetch(this.$appConfig.backendApiUrl + this.$appConfig.endpointCategory)
+        .then(response => response.json())
+        .then(json => {
+          this.categories = json.map(category => ({
+            id: category.id,
+            title: category.name,
+            contents: category.lessons.map(lesson =>({
+              id: lesson.id,
+              title: lesson.name,
+            })),
+          }))
+        })
+        .catch(error => {
+          this.error = 'Could not load categories: ' + error
+          console.error(this.error)
+        })
+        .finally(() => this.loading = false)
+    }
   }
 }
 </script>
