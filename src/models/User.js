f6618718bde88f33e797201c1825a2cb1caed69e
Bernhard Bliem <bernhard@bliem.xyz>
2020-11-13 17:57:23 +0200

    Added 'resource' method to User model, fixing wrong URLs

--- src/models/User.js
+++ src/models/User.js
@@ -2,10 +2,10 @@ import Model from './Model'
 import SectionCompletion from './SectionCompletion'
 
 export default class User extends Model {
-  // We don't access to the User resource at the moment; just getting the user on login as response to our POST request
-  //resource() {
-  //  return 'users'
-  //}
+  resource() {
+    return 'users'
+  }
+
   completeSection(sectionId) {
     if(!this.completedSections.includes(sectionId)) {
       new SectionCompletion({user: this.id, section: sectionId})
