96129a0fb2c61bc4e180af76812211dd8703517f
Bernhard Bliem <bernhard@bliem.xyz>
2020-11-23 16:07:45 +0200

    Hide material sidebar on small screens

--- src/views/Material.vue
+++ src/views/Material.vue
@@ -32,47 +32,51 @@ $sidebar-width: 250px;
 $large-screen-width: 600px;
 
 #sidebar {
+  display: none;
+
   @media screen and (min-width: $large-screen-width) {
+    display: block;
     position: fixed; // Fixed sidebar (stay in place on scroll)
     overflow-y: auto;
     height: 100%;
     width: $sidebar-width;
-  }
-  margin: 0;
-  padding: 10px;
-  /*overflow-x: hidden; [> Disable horizontal scroll <]*/
 
-  ul.lesson-list {
-    list-style-type: none;
-    padding: 0;
     margin: 0;
-  }
+    padding: 10px;
+    /*overflow-x: hidden; [> Disable horizontal scroll <]*/
 
-  a {
-    display: block;
-    padding: 16px;
-    color: $shadow;
-    border-left: .25rem solid transparent;
-    transition: background-color .3s ease;
-    padding: .35rem 1.5rem .35rem 1.25rem;
-
-    &:hover {
-      background: $cultured;
-      text-decoration: none;
+    ul.lesson-list {
+      list-style-type: none;
+      padding: 0;
+      margin: 0;
     }
 
-    &.router-link-active {
-      border-left-color: $shadow;
+    a {
+      display: block;
+      padding: 16px;
+      color: $shadow;
+      border-left: .25rem solid transparent;
+      transition: background-color .3s ease;
+      padding: .35rem 1.5rem .35rem 1.25rem;
+
+      &:hover {
+        background: $cultured;
+        text-decoration: none;
+      }
+
+      &.router-link-active {
+        border-left-color: $shadow;
+      }
     }
-  }
 
-  .category-link {
-    font-size: 1.5rem;
-    font-weight: 700;
-  }
+    .category-link {
+      font-size: 1.5rem;
+      font-weight: 700;
+    }
 
-  .lesson-link {
-    font-size: 1.1rem;
+    .lesson-link {
+      font-size: 1.1rem;
+    }
   }
 }
 
